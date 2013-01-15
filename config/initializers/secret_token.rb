96255c1a2e178547234628543efed137ba8d7152
fancyoung <siyang1982@msn.com>
2013-01-15 16:21:38 +0800

    rails + mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaveServer::Application.config.secret_token = '25970b618814ff7e208e72f64d2fed986854d29220c7e8eafb19ac6a65b9b2e5fbca4c28db20bb47e6926fc685f91cd2ec0a7bafc40368d5af639c05a2bb4558'
